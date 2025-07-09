9da811c90ac11fbcc2d5c31e97d4b73d4a2dc3e4
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-07-09 22:36:42 +0600

    nested routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Route, Routes } from 'react-router';
 import Home from '../pages/Home/Home';
 import About from '../pages/About';
+import Products from '../pages/Products/Products';
+import MainLayout from '../layouts/MainLayout';
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route index element={<Home/>}></Route>
-            <Route path='about' element={<About/>}/>
+            <Route element={<MainLayout/>}>
+                <Route path='/' element={<Home/>}></Route>
+                <Route path='products' element={<Products/>}></Route>
+                <Route path='about' element={<About/>}></Route>
+            </Route>
         </Routes>
     );
 };
