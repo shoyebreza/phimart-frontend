4df5d8355740f4949226fd6dcf13dd34d4e939c6
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-07-13 22:09:34 +0600

    tailwind, daisyui

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, NavLink } from 'react-router';
 
 const Navbar = () => {
     return (
         <div>
-            <Link to="/">Home</Link>
+            {/* <Link to="/">Home</Link>
             <Link to="/products">Product</Link>
-            <Link to="/about">About</Link>
+            <Link to="/about">About</Link> */}
+            <NavLink to="/" className={({isActive})=> (isActive ? 'text-blue-700':'')}>Home</NavLink>
+            <NavLink to="/products" className={({isActive})=> (isActive ? 'text-blue-700':'')}>Product</NavLink>
+            <NavLink to="/about" className={({isActive})=> (isActive ? 'text-blue-700':'')}>About</NavLink>
         </div>
     );
 };
