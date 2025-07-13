ef62ed399bc8c0ea2b5317a929ad24849fb86f8d
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-07-13 21:47:28 +0600

    link

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Navbar = () => {
     return (
         <div>
-            <h1>Nav bar</h1>
+            <Link to="/">Home</Link>
+            <Link to="/products">Product</Link>
+            <Link to="/about">About</Link>
         </div>
     );
 };
