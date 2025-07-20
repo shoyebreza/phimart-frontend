f4de3e39d14626d11b467e370ddbb2718924104e
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-07-20 22:04:40 +0600

    responsive

--- src/components/carousel/CarouselSlide.jsx
+++ src/components/carousel/CarouselSlide.jsx
@@ -1,24 +1,34 @@
+import bgImg from "../../assets/images/banner-image-bg.jpg";
 
-import bgImg from "../../assets/images/banner-image-bg.jpg"
+const CarouselSlide = ({ title, subtitle, image }) => {
+  return (
+    <section
+      className="w-full h-[650px] bg-cover bg-center flex justify-center items-center px-4 md:px-8"
+      style={{ backgroundImage: `url(${bgImg})` }}
+    >
+      <div className="max-w-6xl w-full flex flex-col md:flex-row items-center justify-between px-4 md:px-8">
+        {/* Left Content  */}
+        <div className="w-full md:w-1/2 text-center md:text-left mb-8 md:mb-0">
+          <h1 className="text-4xl md:text-5xl font-bold text-gray-900">
+            {title}
+          </h1>
+          <p className="text-gray-600 my-4">{subtitle}</p>
+          <button className="btn btn-secondary px-6 py-3 rounded-full shadow-md">
+            Shop Product
+          </button>
+        </div>
 
-const CarouselSlide = ({title, subtitle, image}) => {
-    return (
-        <section className="w-full h-[650px] bg-cover bg-center flex justify-center items-center"  style={{backgroundImage:`url(${bgImg})`}}>
-            <div className="flex item-center justify-between max-w-6xl px-8">
-                {/* left content */}
-                <div>
-                    <h1 className="text-5xl font-bold text-gray-900">{title}</h1>
-                    <p className="text-gray-500 my-4">{subtitle}</p>
-                    <button className='btn btn-secondary px-6 py-3 rounded-full shadow-md'>Shop product</button>
-                </div>
-
-                {/*  right content */}
-                <div className="w-1/2 flex justify-center">
-                    <img className="max-wd-md drop-shadow-lg" src={image} alt="image" />
-                </div>
-            </div>
-        </section>
-    );
+        {/* Right Image  */}
+        <div className="w-full md:w-1/2 flex justify-center">
+          <img
+            className="max-w-full md:max-w-md drop-shadow-lg"
+            src={image}
+            alt=""
+          />{" "}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default CarouselSlide;
