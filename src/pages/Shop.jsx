367f83eea2e46772cf8c28c23eea70bca4013404
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-07-26 21:52:08 +0600

    shop start

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,12 @@
+import ShopPage from "../components/shop/ShopPage";
+
+
+const Shop = () => {
+  return (
+    <>
+      <ShopPage/>
+    </>
+  );
+};
+
+export default Shop;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router';
 import Home from '../pages/Home';
 import About from '../pages/About';
+import Shop from '../pages/Shop';
 import MainLayout from '../layouts/MainLayout';
 import Slide from '../components/carousel/CarouselSlide';
 
@@ -11,8 +12,10 @@ const AppRoutes = () => {
             <Route element={<MainLayout/>}>
                 <Route path='/' element={<Home/>}></Route>
                 <Route path='about' element={<About/>}></Route>
+                <Route path='shop' element={<Shop/>}></Route>
             </Route>
         </Routes>
+        
         
         
     );
