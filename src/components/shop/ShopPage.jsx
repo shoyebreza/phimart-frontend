9c5352f17e3677d238ec213e0a93ffd7969e49f3
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-07-28 23:25:33 +0600

    dynamic pagination

--- src/components/shop/ShopPage.jsx
+++ src/components/shop/ShopPage.jsx
@@ -14,17 +14,34 @@ const ShopPage = () => {
 
     useEffect(()=>{
         fetchProducts();
-    },[]);
+    },[currentPage]);
 
-    const fetchProducts = () =>{
-        apiClient.get("/products")
+    /* const fetchProducts = () =>{
+        apiClient.get(`/products/?page=${currentPage}`)
         .then((res)=>{
             setProducts(res.data.results)
             setTotalPages(Math.ceil(res.data.count/ res.data.results.length));
         })
         .catch((error) => console.log(error))
         .finally(() => SetLoading(false));
-    }
+    }; */
+
+    const fetchProducts = async() => {
+        SetLoading(true);
+        try{
+           const response = await apiClient.get(`/products/?page=${currentPage}`);
+            const data = await response.data;
+            setProducts(data.results);
+            setTotalPages(Math.ceil(data.count/ data.results.length)); 
+        }catch(error){
+            console.log(error);
+        }finally{
+            SetLoading(false);
+        }
+        
+    };
+
+
     return (
         <div>
             <ProductList products={products} loading={loading}/>
