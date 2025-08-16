83bf2760ee09be04ae035e5ab9dedfc14f4a9f4b
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2025-08-16 21:29:20 +0600

    account activation

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,37 +1,53 @@
-
-import { Route, Routes } from 'react-router';
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Shop from '../pages/Shop';
-import MainLayout from '../layouts/MainLayout';
-import Slide from '../components/carousel/CarouselSlide';
-import Login from '../pages/Login';
+import { Route, Routes } from "react-router";
+import About from "../pages/About";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import Login from "../pages/Login";
 import Register from "../pages/Register";
-import Dashboard from '../pages/Dashboard';
-import PrivateRoute from '../components/PrivateRoute';
+import Dashboard from "../pages/Dashboard";
+import PrivateRoute from "../components/PrivateRoute";
+import ActivateAccount from "../components/Registration/ActivateAccount";
+import DashboardLayout from "../layouts/DashboardLayout";
+import Profile from "../pages/Profile";
+import ProductDetail from "../pages/ProductDetail";
+import Cart from "../pages/Cart";
+import Orders from "../pages/Orders";
+import PaymentSuccess from "../pages/PaymentSuccess";
+import AddProduct from "../pages/AddProduct";
 
 const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route element={<MainLayout/>}>
-                <Route path='/' element={<Home/>}></Route>
-                <Route path='about' element={<About/>}></Route>
-                <Route path='shop' element={<Shop/>}></Route>
-                <Route path='login' element={<Login/>}></Route>
-                <Route path="register" element={<Register />} />
-                <Route path='/dashboard' element={
-                    <PrivateRoute>
-                        <Dashboard />
-                    </PrivateRoute>
-                }>
+  return (
+    <Routes>
+      {/* Public Routes  */}
 
-                </Route>
-            </Route>
-        </Routes>
-        
-        
-        
-    );
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="activate/:uid/:token" element={<ActivateAccount />} />
+        <Route path="shop/:productId" element={<ProductDetail />} />
+      </Route>
+      {/* Private Routes  */}
+      <Route
+        path="dashboard"
+        element={
+          <PrivateRoute>
+            <DashboardLayout />
+          </PrivateRoute>
+        }
+      >
+        <Route index element={<Dashboard />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="orders" element={<Orders />} />
+        <Route path="payment/success/" element={<PaymentSuccess />} />
+        <Route path="products/add" element={<AddProduct />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default AppRoutes;
